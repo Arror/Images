511dd1d25054ea79eabb8ef013b1f77460f29f8b
Arror <hallo.maqiang@gmail.com>
2017-04-05 00:33:50 +0800

    Update sample

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import Images
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let i1 = AnyIterator<URL>([URL(string: "http://github.com/logo.png")!].makeIterator())
+        
+        self.imageView.img.setImage(iterator: i1)
+        
+        let i2 = AnyIterator<URL>([URL(string: "http://github.com/logo.png")!].makeIterator())
+        
+        self.contentView.layer.img.setImage(iterator: i2)
     }
 }
 
