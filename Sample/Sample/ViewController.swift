481f97a321e0ff789ef7b8893d5cbe4a3ae69e8c
Arror <hallo.maqiang@gmail.com>
2017-04-11 22:22:29 +0800

    Update sample

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -20,7 +20,18 @@ class ViewController: UIViewController {
     
     @IBAction func setImageButtonTapped(_ sender: UIBarButtonItem) {
         
-        self.imageView.img.setImage(urlStrings: ["https://i.ezbuy.sg/Frc0C3N6KjH9bWD7aOGJFWqJuJVn"])
+        let extensionInfo = ImageURLInfo(mode: .m0)
+        
+        let width = Int(self.imageView.bounds.width)
+        let height = Int(self.imageView.bounds.height)
+        
+        let info = extensionInfo.makeQiniuImageURLExtension(w: width, h: height)
+        
+        let urlString = "https://i.ezbuy.sg/Frc0C3N6KjH9bWD7aOGJFWqJuJVn?\(info)"
+        
+        print(urlString)
+        
+        self.imageView.img.setImage(urlStrings: [urlString])
     }
 }
 
